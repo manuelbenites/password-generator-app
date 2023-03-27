76577c69b2800b7de177c625520632a6b5a27b66
Manuel <benitessolismanuel@gmail.com>
2023-03-27 04:02:43 -0500

    added functionality to add numbers, symbols, lowercase and uppercase letters

--- src/App.jsx
+++ src/App.jsx
@@ -1,78 +1,155 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { generateString } from "./lib/functions.js"
+import IconCheck from "./components/IconCheck.jsx"
 import IconCopy from "./components/IconCopy.jsx"
 import IconArrowRight from "./components/IconArrowRight.jsx"
 
 export default function App() {
-	const everyValues =
-		"0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-	const everyValuesArr = everyValues.split("")
+	const [everyValues, setEveryValues] = useState("")
+	const [everyValuesArr, setEveryValuesArr] = useState([])
 	const [minLengthResult, setMinLengthResult] = useState(10)
+	const [isItCopied, setIsItCopied] = useState("")
 	const [password, setPassword] = useState("")
 
-	const handleChangeInputs = (e) => {
+	const copyContent = async () => {
+		if (password) {
+			await navigator.clipboard.writeText(password)
+			setIsItCopied("copied")
+			setTimeout(() => {
+				setIsItCopied("")
+			}, 2000)
+		}
+	}
+
+	const handleCheckBoxChange = (e) => {
+		const { value, checked } = e.target
+		if (checked) {
+			setEveryValues(everyValues + value)
+		} else if (!checked && everyValues) {
+			setEveryValues(everyValues.split(value).join(""))
+		}
+	}
+
+	useEffect(() => {
+		setEveryValuesArr(everyValues.split(""))
+	}, [everyValues])
+
+	const handleChangePasswordLength = (e) => {
 		setMinLengthResult(e.target.value)
 	}
 
 	const submitValues = (e) => {
 		e.preventDefault()
-		setPassword(generateString(everyValuesArr, minLengthResult))
+		if (everyValues) {
+			setPassword(generateString(everyValuesArr, minLengthResult))
+		}
 	}
 
 	return (
 		<div className="flex justify-center items-center w-full h-screen bg-background">
-			<div className="text-gains-boro w-[343px]">
+			<div className="text-gains-boro w-[343px] sm:w-[540px]">
 				<h1 className="font-bold text-center text-light-slate-gray">
 					Password Generator
 				</h1>
-				<div className="flex justify-between items-center p-4 mt-4 h-16 bg-dark-slate-gray">
-					<span className="text-2xl font-bold">{password}</span>
-					<button>
-						<IconCopy />
-					</button>
+				<div className="flex justify-between items-center px-4 mt-4 h-16 sm:px-8 sm:h-20 bg-dark-slate-gray">
+					{password ? (
+						<span className="text-2xl font-bold">{password}</span>
+					) : (
+						<span className="text-2xl font-bold opacity-25 sm:text-[32px]">
+							P4$5W0rD!
+						</span>
+					)}
+					<div className="flex gap-4 justify-center items-center">
+						<span className="hidden text-lg font-bold uppercase sm:block text-pale-green">
+							{isItCopied}
+						</span>
+						<button onClick={copyContent}>
+							<IconCopy className="fill-pale-green hover:fill-[#ffffff] transition-colors" />
+						</button>
+					</div>
 				</div>
-				<div className="p-4 mt-4 bg-dark-slate-gray">
+				<div className="p-4 mt-4 sm:py-8 sm:px-8 sm:mt-6 bg-dark-slate-gray">
 					<div className="flex justify-between items-center">
-						<span className="font-bold">Character Length</span>
-						<span className="text-2xl font-bold text-pale-green">
+						<span className="font-bold sm:text-lg">Character Length</span>
+						<span className="text-2xl font-bold text-pale-green sm:text-[32px]">
 							{minLengthResult}
 						</span>
 					</div>
 					<form onSubmit={submitValues}>
-						<div>
+						<div className="mt-2 mb-8 h-7 sm:mt-4">
 							<input
 								name="length"
 								type="range"
 								min="1"
 								max="20"
-								onChange={handleChangeInputs}
+								value={minLengthResult}
+								className="input-range"
+								onChange={handleChangePasswordLength}
 							/>
 						</div>
-						<div>
-							<input name="uppercase" type="checkbox" />
-							<span className="ml-5 font-bold">Include Uppercase Letters</span>
+						<div className="flex flex-col gap-4 w-full sm:gap-5">
+							<div className="flex relative items-center">
+								<input
+									onChange={handleCheckBoxChange}
+									name="uppercaseLetters"
+									type="checkbox"
+									value="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+									className="border-white appearance-none cursor-pointer outline-none checked:text-white peer w-[18px] h-[18px] border-[2px] checked:bg-pale-green checked:border-pale-green"
+								/>
+								<IconCheck className="absolute font-black pointer-events-none left-[3px] fill-dark-slate-gray peer-checked:fill-deep-black" />
+								<span className="ml-5 font-bold sm:ml-6 sm:text-lg">
+									Include Uppercase Letters
+								</span>
+							</div>
+							<div className="flex relative items-center">
+								<input
+									onChange={handleCheckBoxChange}
+									name="lowercaseLetters"
+									type="checkbox"
+									value="abcdefghijklmnopqrstuvwxyz"
+									className="border-white appearance-none cursor-pointer outline-none checked:text-white peer w-[18px] h-[18px] border-[2px] checked:bg-pale-green checked:border-pale-green"
+								/>
+								<IconCheck className="absolute font-black pointer-events-none left-[3px] fill-dark-slate-gray peer-checked:fill-deep-black" />
+								<span className="ml-5 font-bold sm:ml-6 sm:text-lg">
+									Include Lowercase Letters
+								</span>
+							</div>
+							<div className="flex relative items-center">
+								<input
+									onChange={handleCheckBoxChange}
+									name="numbers"
+									type="checkbox"
+									value="0123456789"
+									className="border-white appearance-none cursor-pointer outline-none checked:text-white peer w-[18px] h-[18px] border-[2px] checked:bg-pale-green checked:border-pale-green"
+								/>
+								<IconCheck className="absolute font-black pointer-events-none left-[3px] fill-dark-slate-gray peer-checked:fill-deep-black" />
+								<span className="ml-5 font-bold sm:ml-6 sm:text-lg">
+									Include Numbers
+								</span>
+							</div>
+							<div className="flex relative items-center mb-8">
+								<input
+									onChange={handleCheckBoxChange}
+									name="symbols"
+									type="checkbox"
+									value="!@#$%^&"
+									className="border-white appearance-none cursor-pointer outline-none checked:text-white peer w-[18px] h-[18px] border-[2px] checked:bg-pale-green checked:border-pale-green"
+								/>
+								<IconCheck className="absolute font-black pointer-events-none left-[3px] fill-dark-slate-gray peer-checked:fill-deep-black" />
+								<span className="ml-5 font-bold sm:ml-6 sm:text-lg">
+									Include Symbols
+								</span>
+							</div>
 						</div>
-						<div>
-							<input name="lowercase" type="checkbox" />
-							<span className="ml-5 font-bold">Include Lowercase Letters</span>
-						</div>
-						<div>
-							<input name="numbers" type="checkbox" />
-							<span className="ml-5 font-bold">Include Numbers</span>
-						</div>
-						<div className="mb-8">
-							<input name="symbols" type="checkbox" />
-							<span className="ml-5 font-bold">Include Symbols</span>
-						</div>
-						<div className="flex justify-between items-center px-4 mb-4 h-14 uppercase bg-deep-black">
+						<div className="flex justify-between items-center px-4 mb-4 h-14 uppercase sm:mb-8 sm:h-16 bg-deep-black">
 							<h1 className="text-lg font-bold text-light-slate-gray">
 								strength
 							</h1>
 							<div className="text-lg font-bold">medium</div>
 						</div>
-						<button className="flex justify-center items-center w-full h-14 font-bold uppercase bg-pale-green text-dark-slate-gray">
-							<h1 className="mr-4">generate</h1>
-							<IconArrowRight className="fill-dark-slate-gray" />
+						<button className="flex justify-center items-center w-full h-14 font-bold uppercase border-2 transition-colors sm:h-16 border-pale-green bg-pale-green text-dark-slate-gray hover:fill-pale-green hover:bg-dark-slate-gray hover:text-pale-green">
+							<h1 className="mr-4 sm:text-lg">generate</h1>
+							<IconArrowRight className="fill-inherit" />
 						</button>
 					</form>
 				</div>
--- src/components/IconCheck.jsx
+++ src/components/IconCheck.jsx
@@ -1,7 +1,10 @@
 export default function IconCheck(props) {
 	return (
-		<svg width="14" height="12" xmlns="http://www.w3.org/2000/svg" {...props}>
-			<path fill={props.fill} d="M1 5.607 4.393 9l8-8" />
+		<svg width="12" height="12" viewBox="0 0 24 24" {...props}>
+			<path
+				fill={props.fill}
+				d="M9 21.035l-9-8.638 2.791-2.87 6.156 5.874 12.21-12.436 2.843 2.817z"
+			/>
 		</svg>
 	)
 }
